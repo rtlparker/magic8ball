93f33867fa8c0d44d944cfe74a64481abc3d4be7
rtlparker <140985704+rtlparker@users.noreply.github.com>
2023-07-31 01:20:48 +0100

    Version 1.0 github release

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RTLParker.xyz - Projects</title>
+    <link rel="stylesheet" href="styles_projects.css">
+</head>
+
+      <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J8HRRG3FPN"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-J8HRRG3FPN');
+    </script>
+
+<body>
+    <header class="header">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/projects/index.php">Projects</a></li>
+                </ul>
+            </nav>
+
+            <h1 class="title">RTLParker.xyz</h1>
+            <p class="subtitle">A playground for web developers</p>
+            <div class="github-icon">
+                <a href="https://github.com/rtlparker" target="_blank" rel="noopener noreferrer">
+                    <i class="fab fa-github"></i>
+                </a>
+            </div>
+        </div>
+    </header>
+
+    <main class="main">
+        <div class="container">
+            <section class="project-list">
+                <h2>Projects</h2>
+                <div class="project">
+                    <h3>Turnover Calculator</h3>
+                    <p>A turnover calculator that lets you input monthly income and see how much that is after 12 months.</p>
+                    <a href="turnover-calculator.rtlparker.xyz">View Project</a>
+                    <a class="github-link" href="https://github.com/rtlparker/turnover-calculator" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-github"></i> GitHub
+                    </a>
+                </div>
+                <div class="project">
+                    <h3>Magic 8 ball</h3>
+                    <p>A basic magic 8 ball.</p>
+                    <a href="https://magic8.rtlparker.xyz">View Project</a>
+                    <a class="github-link" href="https://github.com/rtlparker/turnover-calculator" target="_blank" rel="noopener noreferrer">
+                        <i class="fab fa-github"></i> GitHub
+                    </a>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> RTLParker.xyz | All rights reserved.</p>
+        </div>
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js"></script>
+</body>
+
+</html>
